1dc59e2b6c3c4655cb8f80055798469e999684d7
Sameer Shelar <workingshelar@gmail.com>
2023-03-05 17:04:13 +0530

    -Added Resource.kt class -Added TranslateUseCase.kt

---
+++ shared/src/commonMain/kotlin/com/sameershelar/translator/translate/domain/translate/TranslateUseCase.kt
@@ -0,0 +1,37 @@
+package com.sameershelar.translator.translate.domain.translate
+
+import com.sameershelar.translator.core.domain.language.Language
+import com.sameershelar.translator.core.domain.util.Resource
+import com.sameershelar.translator.translate.domain.history.HistoryDataSource
+import com.sameershelar.translator.translate.domain.history.HistoryItem
+
+class TranslateUseCase(
+    private val client: TranslateClient,
+    private val historyDataSource: HistoryDataSource,
+) {
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language,
+    ): Resource<String> {
+        try {
+            val translatedText = client.translate(
+                fromLanguage = fromLanguage,
+                fromText = fromText,
+                toLanguage = toLanguage,
+            )
+            historyDataSource.insertHistoryItem(
+                item = HistoryItem(
+                    id = null,
+                    fromLanguageCode = fromLanguage.langCode,
+                    fromText = fromText,
+                    toLanguageCode = toLanguage.langCode,
+                    toText = translatedText,
+                )
+            )
+        } catch (e: TranslateException) {
+            e.printStackTrace()
+            Resource.Error<TranslateException>(throwable = e)
+        }
+    }
+}
